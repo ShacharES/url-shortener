f103bb34ea2b90178a311f2e0a6a9a263653e05e
shahar.even-shoshan <shachar987@gmail.com>
2025-04-02 11:32:35 +0300

    Add unit testing support for UrlShortener class with CMake configuration

---
+++ build/CMakeFiles/UnitTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnitTest.dir/src/UrlShortener.cpp.o"
+  "CMakeFiles/UnitTest.dir/src/UrlShortener.cpp.o.d"
+  "CMakeFiles/UnitTest.dir/test/UnitTest.cpp.o"
+  "CMakeFiles/UnitTest.dir/test/UnitTest.cpp.o.d"
+  "UnitTest"
+  "UnitTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
