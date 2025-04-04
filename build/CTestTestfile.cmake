c1b13478e718d02078d62e7410707d8d2fcebdf0
shahar.even-shoshan <shachar987@gmail.com>
2025-04-04 18:24:16 +0300

    Remove build artifacts and temporary files to clean up the repository

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shahar/git/projects/aka/url-shortener
-# Build directory: /home/shahar/git/projects/aka/url-shortener/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UrlShortenerTests "/home/shahar/git/projects/aka/url-shortener/build/UnitTest")
-set_tests_properties(UrlShortenerTests PROPERTIES  _BACKTRACE_TRIPLES "/home/shahar/git/projects/aka/url-shortener/CMakeLists.txt;27;add_test;/home/shahar/git/projects/aka/url-shortener/CMakeLists.txt;0;")
